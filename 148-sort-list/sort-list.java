6966b688cff86935c56cff851087ca0fa1998975
priyankamallya <120215272+priyankamallya@users.noreply.github.com>
2024-08-11 21:55:31 +0530

    Time: 9 ms (92.21%) | Memory: 56.5 MB (66.68%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -11,18 +11,46 @@
 class Solution {
     public ListNode sortList(ListNode head) {
         if(head == null || head.next==null) return head;
-        ArrayList<Integer> arr = new ArrayList<>();
-        ListNode temp = head;
-        while(temp!=null){
-            arr.add(temp.val);
-            temp=temp.next;
+        ListNode middle = findMiddle(head);
+        //divide the list into two halves
+        ListNode right = middle.next;
+        middle.next = null;
+        ListNode left = head;
+        left = sortList(left);
+        right = sortList(right);
+        return mergeTwoSortedLL(left,right);
+        
+    }
+    public ListNode findMiddle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
         }
-        Collections.sort(arr);
-        temp = head;
-        for(int i=0;i<arr.size();i++){
-            temp.val = arr.get(i);
+        return slow;
+    }
+    public ListNode mergeTwoSortedLL(ListNode list1, ListNode list2){
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while(list1!=null && list2!=null){
+            if(list1.val <= list2.val){
+                temp.next = list1;
+                list1 = list1.next;
+            }else{
+                temp.next = list2;
+                list2 = list2.next;
+            }
             temp = temp.next;
         }
-        return head;
+        if(list1!=null){
+            temp.next = list1;
+            temp = temp.next;
+        }else{
+            temp.next = list2;
+            temp = temp.next;
+        }
+        return dummy.next;
     }
 }
