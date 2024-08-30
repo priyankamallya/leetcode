64efe0aafb47cb2e53ca4c4571864a1e0c49c5a0
priyankamallya <120215272+priyankamallya@users.noreply.github.com>
2024-08-30 15:02:43 +0530

    Time: 456 ms (13.29%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select e1.name
 from employee e1
-inner join employee e2 on e1.id = e2.managerID
-group by e2.managerID 
-having count(e2.managerID)>=5;
+inner join employee e2 on e1.id = e2.managerId
+group by e2.managerId 
+having count(e2.managerId)>=5;
