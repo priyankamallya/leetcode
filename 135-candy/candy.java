415436e9603960ff995142480e440f5efda9d6fe
priyankamallya <120215272+priyankamallya@users.noreply.github.com>
2024-09-26 13:11:50 +0530

    Time: 3 ms (80.21%) | Memory: 46.5 MB (6.16%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,24 +1,30 @@
 class Solution {
     public int candy(int[] ratings) {
+        int i=1;
+        int sum=1;
         int n = ratings.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        //only considering the left neighbours
-        left[0]=1;
-        for(int i=1;i<n;i++){
-            if(ratings[i-1]<ratings[i]){
-                left[i]= left[i-1]+1;
-            }else left[i]=1;
-        }
-        right[n-1]=1;
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i+1]<ratings[i]){
-                right[i] = right[i+1]+1;
-            }else right[i]=1;
-        }
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=Math.max(left[i],right[i]);
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum+=1;
+                i++;
+                continue;
+            }
+            int peak=1;
+            //assuming it is a high slope
+            while(i<n && ratings[i-1]<ratings[i]){
+                peak+=1;
+                sum+=peak;
+                i++;
+            }
+            int down =0;
+            //assuming it is a downward slope
+            while(i<n && ratings[i]<ratings[i-1]){
+                down+=1;
+                sum+=down;
+                i++;
+            }
+            down++;
+            if(down>peak) sum+=(down-peak);
         }
         return sum;
     }
